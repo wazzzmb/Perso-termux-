2d55c673a9461db4c0159f10b7d5fc1dbd852997
wazzzmb <mehdibouslah55@gmail.com>
2026-01-10 10:25:26 +0100

    Update banner.py

--- banner.py
+++ banner.py
@@ -1,40 +1,33 @@
 import os
 import time
 
-# Fichier bashrc de Termux
 BASHRC = os.path.expanduser("~/.bashrc")
 
-# Ton pseudo
 pseudo = input("Ton pseudo pour Termux : ")
 
-# Couleurs ANSI arc-en-ciel (violet, orange, rose, cyan)
 colors = [
     "\033[95m", # violet
     "\033[33m", # orange
-    "\033[91m", # rouge/rose
+    "\033[91m", # rose
     "\033[96m", # cyan
 ]
 
-# Bannière ASCII simple
 banner = [
-    __________.__                                                
-\______   \__| ____   _______  __ ____   ____  __ __   ____  
- |    |  _/  |/ __ \ /    \  \/ // __ \ /    \|  |  \_/ __ \ 
- |    |   \  \  ___/|   |  \   /\  ___/|   |  \  |  /\  ___/ 
- |______  /__|\___  >___|  /\_/  \___  >___|  /____/  \___  >
-        \/        \/     \/          \/     \/            \/ 
+"________.__",
+"\\______   \\__| ____   _______  __ ____   ____  __ __   ____",
+" |    |  _/  |/ __ \\ /    \\  \\/ // __ \\ /    \\|  |  \\_/ __ \\",
+" |    |   \\  \\  ___/|   |  \\   /\\  ___/|   |  \\  |  /\\  ___/",
+" |______  /__|\\___  >___|  /\\_/  \\___  >___|  /____/  \\___  >",
+"        \\/        \\/     \\/          \\/     \\/            \\/"
 ]
 
 def write_banner():
     with open(BASHRC, "w") as f:
         f.write("clear\n")
         f.write("echo ''\n")
-        # Couleur arc-en-ciel
         for i, line in enumerate(banner):
             color = colors[i % len(colors)]
-            # Bash ANSI color
             f.write(f"echo -e '{color}{line}\\033[0m'\n")
-        # Message d’accueil avec pseudo
         f.write(f"echo -e '\\033[93mBienvenue {pseudo} dans Termux !\\033[0m'\n")
         f.write("echo ''\n")
 
